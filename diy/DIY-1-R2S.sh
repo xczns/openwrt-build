4f24adc3d2d5cc8143b762d32dfca44fce7e022a
xcz <zyb183328@email.swu.edu.cn>
2024-07-05 12:58:38 +0800

    更新-0705-1258

--- diy/DIY-1-R2S.sh
+++ diy/DIY-1-R2S.sh
@@ -1,26 +1,9 @@
 # 此脚本用处是：添加第三方插件
 #=========================================================================================================================
-
-
-# 1-添加 ShadowSocksR Plus+ 插件
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 2-添加 PowerOff 关机插件
-# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
-
-# 3-添加 opentomcat 主题
-# git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
-# 4-添加 OpenClash 插件
-# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-
-# 5-添加 PassWall 插件
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
-# 6-自用插件源
+# 自用插件源
 echo "src-git danshui1 https://github.com/281677160/openwrt-package.git" >> "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo "src-git passwall1 https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> "feeds.conf.default"
 echo "src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages" >> "feeds.conf.default"
+echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master" >> "feeds.conf.default"
