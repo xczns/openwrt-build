d29d94d481273e8761fb656baa832f79869b3990
xcz <zyb183328@email.swu.edu.cn>
2024-07-08 21:27:15 +0800

    更新 DIY-1-x86-personal.sh

--- diy/DIY-1-x86-personal.sh
+++ diy/DIY-1-x86-personal.sh
@@ -1,9 +1,10 @@
 # 此脚本用处是：添加第三方插件
 #=========================================================================================================================
 # 自用插件源
-echo "src-git danshui1 https://github.com/281677160/openwrt-package.git" >> "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-echo "src-git passwall1 https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> "feeds.conf.default"
-echo "src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages" >> "feeds.conf.default"
-echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master" >> "feeds.conf.default"
+# echo "src-git danshui1 https://github.com/281677160/openwrt-package.git" >> "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# echo "src-git passwall1 https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
+# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> "feeds.conf.default"
+# echo "src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages" >> "feeds.conf.default"
+# echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master" >> "feeds.conf.default"
+echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> "feeds.conf.default"
